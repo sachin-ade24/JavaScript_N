da48c6cf048c4efa71cd4a5c97aac08373ad6c36
sachin-ade24 <120191253+sachin-ade24@users.noreply.github.com>
2025-01-26 13:48:52 +0530

    Added objectCreate.js file

---
+++ s10/objectCreate.js
@@ -0,0 +1,73 @@
+//1. Object Literals: {}
+const user = {
+  name: "Michael",
+  age: 25,
+  country: "USA",
+  Occupation: "Engineer"
+};
+console.log(user.name); //Michael
+console.log(user.Occupation); //Engineer
+
+//2. Constructor function
+function Car(brand, model, price) {
+  this.brand = brand;
+  this.model = model;
+  this.price = price;
+}
+const c1 = new Car("BMW", "520d", 10000);
+console.log(`Brand: ${c1.brand} | Model: ${c1.model} | Price: ${c1.price}`);
+//Brand: BMW | Model: 520d | Price: 10000
+const c2 = new Car("Audi", "Q7", 15000);
+console.log(`Brand: ${c2.brand} | Model: ${c2.model} | Price: ${c2.price}`);
+//Brand: Audi | Model: Q7 | Price: 15000
+
+//3. Class style:
+class Customer {
+  constructor(name, product) {
+    //instance variable --> this.name; local or constructor variable: name
+    this.name = name;
+    //instance variable --> this.product; local or constructor variable: product
+    this.product = product;
+  }
+
+  addToCart() {
+    console.log(`${this.product} added to cart.`);
+  }
+}
+//create instance/object of the class
+const cust1 = new Customer("Naveen", "Macbook pro");
+console.log(`${cust1.name} has purchased ${cust1.product}.`); //Naveen has purchased Macboock pro.
+cust1.addToCart(); //Macboock pro added to cart.
+
+//4. Object.create() method with some prototype
+//creating prototype:
+const employeePrototype = {
+  printInfo: function () {
+    console.log(`Hello, the employee name is ${this.name}.`);
+  }
+};
+const e1 = Object.create(employeePrototype);
+e1.name = "Tom";
+e1.printInfo(); //Hello, the employee name is Tom.
+
+//4. using the Factory Function: returns ab object.
+function createDepartment(deptName, hod) {
+  return {
+    deptName: deptName,
+    hod: hod,
+    getDepartmentInfo: function () {
+      console.log(
+        `The department name is ${this.deptName} and hod is ${this.hod}`
+      );
+    }
+  };
+}
+//createDepartment("Physics", "H C Verma");
+//this will not print anything on the console,
+//because we need to call the function 'getDepartmentInfo' to print info on the console
+
+const dept1 = createDepartment("Physics", "H C Verma");
+const dept2 = createDepartment("Maths", "Shri Ramanujan");
+dept1.getDepartmentInfo(); //The department name is Physics and hod is H C Verma
+dept2.getDepartmentInfo(); //The department name is Maths and hod is Shri Ramanujan
+console.log(`Departrmrnt: ${dept1.deptName} | HOD: ${dept1.hod}`); // Departrmrnt: Physics | HOD: H C Verma
