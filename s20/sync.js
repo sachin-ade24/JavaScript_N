2baaa6605f424d1e1401e549f3a9fef9c22b885d
sachin-ade24 <120191253+sachin-ade24@users.noreply.github.com>
2025-01-28 00:36:20 +0530

    updates

--- s20/sync.js
+++ s20/sync.js
@@ -3,7 +3,7 @@ const data = fs.readFileSync("info.txt", "utf-8"); //utf-8 --> encoding type
 //task1
 console.log(data); //Hi, I am learning javaScript.
 //task2
-console.log("Java is awesome too.."); //Java is awesome too..
+console.log("Java is awesome.."); //Java is awesome too..
 //o/p:
 //task1 must be executed first and then 2nd
 //Hi, I am learning javaScript.
